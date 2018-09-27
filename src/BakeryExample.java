b5383e4afc8d3ff2ac0ac8df703e930411e6b231
Stephen Vondenstein <stephen@vondenstein.com>
2018-09-26 22:21:05 -0500

    Thread count is now determined by the user via the command line.

--- src/BakeryExample.java
+++ src/BakeryExample.java
@@ -1,13 +1,28 @@
 /*
  * Stephen Vondenstein
  *
- * Setup and example execution of Lamport's Bakery Algorithm with 10 threads
- * When running this code on my machine, it took about 90 seconds to execute on average
+ * Setup and example execution of Lamport's Bakery Algorithm with n threads
+ * When running this code on my machine, it took about 90 seconds to execute with n = 10.
  */
+import java.util.Scanner;
+
 public class BakeryExample {
 
     public static void main(String[] args) {
-        int n = 10; //number of threads
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of threads in the bakery: ");
+        int n = 0;
+
+        //Ensure that we always get an integer > 0 for thread count
+        while (n < 1) {
+            System.out.print("Enter the number of threads (n > 0) in the bakery: ");
+            try {
+                n = Integer.parseInt(scanner.next()); //number of threads
+            } catch (NumberFormatException e) {
+                System.out.println("Whoops! Looks like you didn't enter an integer greater than 0.");
+            }
+        }
+
         Bakery b = new Bakery(n);
         //From example code:
         class Shared {
